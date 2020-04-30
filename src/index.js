b78191288219e9ad0b163b2122155075efaee7f8
Alexey Volkov <grulfex@gmail.com>
2020-04-30 17:38:24 +0300

    Add event listener for home button

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import loadWelcome from './welcome';
 import renderNavbar from './navbar';
 import renderFooter from './footer';
+import renderHome from './navigation';
 
 renderNavbar();
+loadWelcome();
 renderFooter();
+renderHome();
