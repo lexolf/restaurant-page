88a784f1f451c80067ef4c3c58874ec7a3e4296a
Alexey Volkov <grulfex@gmail.com>
2020-05-01 23:06:56 +0300

    Add sources for menu items

--- src/footer.js
+++ src/footer.js
@@ -11,41 +11,43 @@ const renderFooter = () => {
     madeBy.id = "made-by";
     madeBy.textContent = "Made by Lexolf";
     footer.appendChild(madeBy);
-    let imageSources = document.createElement("ul");
+    let imageSources = document.createElement("div");
     imageSources.id = "image-sources";
     footer.appendChild(imageSources);
-    let imageSourcesLabel = document.createElement("li");
+    let imageSourcesLabel = document.createElement("div");
     imageSourcesLabel.id = "image-sources-label";
     imageSourcesLabel.textContent = "Image sources: ";
     imageSources.appendChild(imageSourcesLabel);
-    let source1 = document.createElement("li");
-    imageSources.appendChild(source1);
-    let source1link = document.createElement("a");
-    source1link.classList = "source";
-    source1link.href = "https://upload.wikimedia.org/wikipedia/commons/f/f9/Bread_%28example%29.svg";
-    source1link.textContent = "[1]";
-    source1.appendChild(source1link);
-    let source2 = document.createElement("li");
-    imageSources.appendChild(source2);
-    let source2link = document.createElement("a");
-    source2link.classList = "source";
-    source2link.href = "https://upload.wikimedia.org/wikipedia/commons/9/92/Wikimania2019_leaves_stem_icon.svg";
-    source2link.textContent = "[2]";
-    source2.appendChild(source2link);
-    let source3 = document.createElement("li");
-    imageSources.appendChild(source3);
-    let source3link = document.createElement("a");
-    source3link.classList = "source";
-    source3link.href = "https://unsplash.com/photos/3DDvgICjBYg";
-    source3link.textContent = "[3]";
-    source3.appendChild(source3link);
-    let source4 = document.createElement("li");
-    imageSources.appendChild(source4);
-    let source4link = document.createElement("a");
-    source4link.classList = "source";
-    source4link.href = "https://unsplash.com/photos/rsWZ-P9FbQ4";
-    source4link.textContent = "[4]";
-    source4.appendChild(source4link);
+
+    let linksArray = [
+        "https://upload.wikimedia.org/wikipedia/commons/f/f9/Bread_%28example%29.svg",
+        "https://upload.wikimedia.org/wikipedia/commons/9/92/Wikimania2019_leaves_stem_icon.svg",
+        "https://unsplash.com/photos/3DDvgICjBYg",
+        "https://unsplash.com/photos/rsWZ-P9FbQ4",
+        "https://unsplash.com/photos/m9pzwmxm2rk",
+        "https://unsplash.com/photos/vDaFiSTMCgQ",
+        "https://unsplash.com/photos/5XwDDNRLgMs",
+        "https://unsplash.com/photos/vvEChjRq2sk",
+        "https://unsplash.com/photos/gTuGowPjzpo",
+        "https://unsplash.com/photos/cWXibBbXx44",
+        "https://unsplash.com/photos/RbqgLewxyXo",
+        "https://unsplash.com/photos/aFxBAwwfSnc",
+        "https://unsplash.com/photos/zUNs99PGDg0",
+        "https://unsplash.com/photos/nBJHO6wmRWw",
+        "https://unsplash.com/photos/vtntD9h1xBw"
+    ]
+
+
+    for(let i in linksArray){
+        let source = document.createElement("div");
+        imageSources.appendChild(source);
+        let sourceLink = document.createElement("a");
+        sourceLink.classList = "source";
+        sourceLink.href = linksArray[i];
+        let index = parseFloat(i) + parseFloat(1);
+        sourceLink.textContent = "[" + index + "]";
+        source.appendChild(sourceLink);
+    }
 }
 
     //     <div id="footer">
@@ -54,9 +56,20 @@ const renderFooter = () => {
     //     <ul id="image-sources">
     //         <li id="image-sources-label">Image sources:</li>
     //         <li><a href="https://upload.wikimedia.org/wikipedia/commons/f/f9/Bread_%28example%29.svg" class="source">[1]</a></li>
-    //         <li><a href="https://upload.wikimedia.org/wikipedia/commons/9/92/Wikimania2019_leaves_stem_icon.svg" class="source">[2]</a></li>
+    //         <li><a href="https://upload.wikimedia.org/wikipedia/commons/f/f9/Bread_%28example%29.svg" class="source">[2]</a></li>
     //         <li><a href="https://unsplash.com/photos/3DDvgICjBYg" class="source">[3]</a></li>
     //         <li><a href="https://unsplash.com/photos/rsWZ-P9FbQ4" class="source">[4]</a></li>
+    //         <li><a href="https://unsplash.com/photos/m9pzwmxm2rk" class="source">[5]</a></li>
+    //         <li><a href="https://unsplash.com/photos/vDaFiSTMCgQ" class="source">[6]</a></li>
+    //         <li><a href="https://unsplash.com/photos/5XwDDNRLgMs" class="source">[7]</a></li>
+    //         <li><a href="https://unsplash.com/photos/vvEChjRq2sk" class="source">[8]</a></li>
+    //         <li><a href="https://unsplash.com/photos/gTuGowPjzpo" class="source">[9]</a></li>
+    //         <li><a href="https://unsplash.com/photos/cWXibBbXx44" class="source">[10]</a></li>
+    //         <li><a href="https://unsplash.com/photos/RbqgLewxyXo" class="source">[11]</a></li>
+    //         <li><a href="https://unsplash.com/photos/aFxBAwwfSnc" class="source">[12]</a></li>
+    //         <li><a href="https://unsplash.com/photos/zUNs99PGDg0" class="source">[13]</a></li>
+    //         <li><a href="https://unsplash.com/photos/nBJHO6wmRWw" class="source">[14]</a></li>
+    //         <li><a href="https://unsplash.com/photos/vtntD9h1xBw" class="source">[15]</a></li>
     //     </ul>
     // </div>
 
